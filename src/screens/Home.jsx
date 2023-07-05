e55b351a2babd214ce4e0db2d7dae4f98a397e2d
Sohaib Abbasi <sohaibabbasiwork@gmail.com>
2023-07-05 13:10:17 +0500

    react native app

---
+++ src/screens/Home.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Header from '../components/Header/Header'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Header />
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
